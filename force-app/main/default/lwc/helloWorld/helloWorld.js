84455e2a6d94eb89606f55a8e84a2bbe3f813b0a
bhanu <bhanu.karkra@gmail.com>
2021-03-28 13:23:14 +0530

    setting up my repo

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+// class is blurprint or template which ontaines attributes and methods
+// class MObile - (size, color, cost), method(calling, cancelling, send)
+// xiomi - let xiomiA1 = new Mobile()
+//xiomiA1.size =
+export default class HelloWorld extends LightningElement {
+    // attribute/fields/properties and behaviour/function/methods
+    name="nikhil"
+    
+    nameHandler(event){ 
+        console.log(event.target.value)
+        this.name = event.target.value
+    }
+}
